25317f9f1b19ac4decf188bc6713894845886623
Sunday <dev.nattapon.s@gmail.com>
2021-12-06 02:24:30 +0700

    add todo list solidity

---
+++ fun/todoList.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Nolicense
+pragma solidity ^0.8.3;
+
+contract TodoList {
+    struct Todo {
+        string text;
+        bool completed;
+    }
+
+    Todo[] public todos;
+
+    function create(string calldata _text) external {
+        todos.push(Todo({text: _text, completed: false}));
+    }
+
+    function updateText(uint256 _index, string calldata _text) external {
+        todos[_index].text = _text;
+    }
+
+    function get(uint256 _index) external view returns (string memory, bool) {
+        Todo memory todo = todos[_index];
+        return (todo.text, todo.completed);
+    }
+
+    function toggleCompled(uint256 _index) external {
+        todos[_index].completed = !todos[_index].completed;
+    }
+}
